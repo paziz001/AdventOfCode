8a9eb8bad57c4c6a27209c854ef140807510ba6e
Philippos Aziz <philaziz92@gmail.com>
2023-12-22 16:30:27 +0100

    Day12 Part 1

---
+++ 2023/Day12/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+var regexRecord = new Regex(@"^(?<conditions>.+) (?<groups>.+)$");
+var fileReader = new StreamReader(new FileStream("input", FileMode.Open));
+var sum = 0;
+while(!fileReader.EndOfStream)
+{
+    var line = await fileReader.ReadLineAsync();
+    var record = regexRecord.Matches(line);
+    var condition = record[0].Groups["conditions"].Value;
+    var groups = record[0].Groups["groups"].Value.Split(',').Select(int.Parse).ToList();
+
+    sum += PrintPermutations(condition);
+    int PrintPermutations(string condition)
+    {
+        var index = condition.IndexOf('?');
+        var regex = new Regex(@"\?");
+        if(regex.IsMatch(condition))
+        {
+            return PrintPermutations(regex.Replace(condition, ".", 1)) +
+                PrintPermutations(regex.Replace(condition, "#", 1));
+        }
+        else if(IsValid(condition))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    bool IsValid(string condition)
+    {
+        var regexBuild = new StringBuilder();
+        regexBuild.Append(@"^\.*");
+        for (var i = 0; i < groups.Count; i++) 
+        {
+            regexBuild.Append(@$"(#{{{groups[i]}}})");
+            if (i < (groups.Count - 1)) 
+            {
+                regexBuild.Append(@"\.+");
+            }
+            else
+            {
+                regexBuild.Append(@"\.*$");
+            }
+        }
+
+        var regexPattern = new Regex(regexBuild.ToString());
+
+        return regexPattern.IsMatch(condition);
+    }
+
+}
+
+Console.WriteLine(sum);
